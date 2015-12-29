e22c0d4e02150eba56889437680239b0a4398597
Ilya Minyaev <ayli.veaynim@gmail.com>
2015-12-29 21:55:11 +0300

    Connection to local db
    Added dump

--- app.r
+++ app.r
@@ -1,17 +1,26 @@
 ## app.R ##
 library(shiny)
-
-#setwd(dirname(sys.frame(1)$ofile))
+library(RMySQL)
 
 source('./ui.r')
 
-server <- function(input, output) { 
+server <- function(input, output, session) {
+  # Insert your user and password
+  con <- dbConnect(MySQL(),
+                   user="root", password="admin",
+                   dbname="acp_dev", host="localhost")
+  
+  
   set.seed(122)
   histdata <- rnorm(500)
-  
   output$plot1 <- renderPlot({
     data <- histdata[seq_len(input$slider)]
     hist(data)
+  })
+  
+  # Cleanup after closing session
+  session$onSessionEnded(function() {
+    dbDisconnect(con)
   })
 }
 
--- install.r
+++ install.r
@@ -1,4 +1,4 @@
 # Install packages
 install.packages("shiny")
 install.packages("shinydashboard")
-
+install.packages("RMySQL")
