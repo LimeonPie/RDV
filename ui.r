ef16eb31b04006e4c03964972487c34734e410e4
Ilya Minyaev <ayli.veaynim@gmail.com>
2016-01-11 18:59:11 +0200

    New UI
    Added tabs, and basic content to them.

--- ui.r
+++ ui.r
@@ -3,189 +3,156 @@ library(shinydashboard)
 
 ui <- dashboardPage(
   skin = "red",
+  
   dashboardHeader(
     title = "Reddit data visualisation", 
-    titleWidth = 400
+    titleWidth = 300
   ),
+  
   dashboardSidebar(
-    width = 400,
+    width = 300,
+    sidebarMenu(
+      menuItem(
+        "Home", 
+        tabName = "default", 
+        icon = icon("home", lib = "glyphicon")
+      ),
+      menuItem(
+        "Configure", 
+        tabName = "conf", 
+        icon = icon("pencil", lib = "glyphicon")
+      ),
+      menuItem(
+        "Plot", 
+        tabName = "plot", 
+        icon = icon("stats", lib = "glyphicon")
+      )
+    ),
+    
     column(
-      width = 12,
-           
-      selectInput(
-        'select', 
-        label = h3("Pattern"), 
-        choices = list(
-          "Comment analysis" = 1, 
-          "Users analysis" = 2,
-          "Subreddit analysis" = 3, 
-          "Subreddit relations" = 4,
-          "Frequency of words" = 5, 
-          selected = "Comment analysis"
-        )
-      ),
-        
-      dateRangeInput("dates", 
-        label = h3("Date Range")
-      ),
-      
-      # sidebarSearchForm(textId = "searchSubreddits", buttonId = "subreddit", label = "Subreddits..."),
-      # allow creation of new items in the drop-down list
-      
-      selectizeInput(
-        'subreddits', 
-        h3("Select subreddit"), 
-        choices = c(
-          'sports', 
-          'politics', 
-          'reddit.com', 
-          'science', 
-          'programming'
-        ), 
-        multiple = TRUE,
-        options = list(
-          create = TRUE, 
-          maxItems = 10000, 
-          placeholder = '/r/...'
-        )
-      ),
-        
-      selectizeInput(
-        'keywords', 
-        h3("Select keywords"), 
-        choices ="",
-        multiple = TRUE,
-        options = list(
-          create = TRUE, 
-          maxItems = 10000, 
-          placeholder = 'Keyword'
-        )
-      ),
-        
-      radioButtons(
-        "radio", 
-        label = h3("Gold"),
-        choices = list(
-          "All" = 1, 
-          "Yes" = 2, 
-          "No" = 3
-        ), 
-        selected = 1
-      ),
-        
-      h3("Upvotes"),
-      
-      numericInput(
-        "upvotes_max", 
-        label = h4("max"), 
-        value = NULL
-      ),
-      numericInput(
-        "upvotes_min", 
-        label = h4("min"), 
-        value = NULL
-      ),
-        
-      h3("Downvotes"),
-      
-      numericInput(
-        "downvotes_max", 
-        label = h4("max"), 
-        value = NULL
-      ),
-      
-      numericInput(
-        "downvotes_min", 
-        label = h4("min"), 
-        value = NULL
-      ),
-        
+      width = 1,
       actionButton(
-        "go", 
-        label = "GO!", 
-        width = "50%"
+        "launchButton", 
+        label = "Release the Kraken!"
       )
     )
   ),
   
   dashboardBody(
-    fluidRow(
-      box(
-        width = 6,
-        selectInput(
-          "select", 
-          label = h3("Pattern"),
-          choices = list(
-            "Comment analysis" = 1, 
-            "Users analysis" = 2,
-            "Subreddit analysis" = 3, 
-            "Subreddit relations" = 4,
-            "Frequency of words" = 5, 
-            selected = 1
+    tabItems(
+      
+      # Default tab content
+      tabItem(
+        tabName = "default",
+        h2("Display here some basic page")
+      ),
+      
+      # Conf tab content
+      tabItem(
+        tabName = "conf",
+        fluidRow(
+          box(
+            title = "Pattern select",
+            selectInput(
+              'patternSelect',
+              label = h3("Pattern"),
+              choices = list(
+                "Comment analysis" = 1, 
+                "Users analysis" = 2,
+                "Subreddit analysis" = 3, 
+                "Subreddit relations" = 4,
+                "Frequency of words" = 5, 
+                selected = "Comment analysis"
+              )
+            )
           )
         ),
         
-        dateRangeInput(
-          "dates", 
-          label = h3("Date Range")
-        )
-      ),
-        
-      box(
-        width = 6,
-        textInput(
-          "text", 
-          label = h3("Keywords")
+        fluidRow(  
+          box(
+            title = "Date select",
+            dateRangeInput(
+              "dates",
+              label = h3("Pattern")
+            )
+          )
         ),
           
-        radioButtons(
-          "radio", 
-          label = h3("Gold"),
-          choices = list(
-            "All" = 1, 
-            "Yes" = 2, 
-            "No" = 3
-          ),
-          selected = 1
-        )
-      )
-    ),
-  
-    fluidRow(
-      box(
-        title = "Upvotes",
-        numericInput(
-          "upvotes_max", 
-          label = h3("max"), 
-          value = NULL
+        fluidRow(
+          box(
+            title = "Subreddits select",
+            selectizeInput(
+              'subredditsSelect',
+              label = h3("Pattern"),
+              choices = c(
+                'sports', 
+                'politics', 
+                'reddit.com', 
+                'science', 
+                'programming'
+              ), 
+              multiple = TRUE,
+              options = list(
+                create = TRUE, 
+                maxItems = 10000, 
+                placeholder = '/r/...'
+              )
+            )
+          )
         ),
-        numericInput(
-          "upvotes_min", 
-          label = h3("min"), 
-          value = NULL
+        
+        fluidRow(
+          box(
+            title = "Keywords input",
+            selectizeInput(
+              'keywordsInput',
+              label = h3("Pattern"),
+              choices ="",
+              multiple = TRUE,
+              options = list(
+                create = TRUE, 
+                maxItems = 10000, 
+                placeholder = 'Keyword'
+              )
+            )
+          )
+        ),
+        
+        fluidRow(  
+          box(
+            title = "Gold status",
+            radioButtons(
+              "isGilded",
+              label = h3("Pattern"),
+              choices = list(
+                "All" = 1, 
+                "Yes" = 2, 
+                "No" = 3
+              ), 
+              selected = 1
+            )
+          )
         )
       ),
       
-      box(
-        title = "Downvotes",
-        numericInput(
-          "downvotes_max", 
-          label = h3("max"), 
-          value = NULL
-        ),
-        numericInput(
-          "downvotes_min", 
-          label = h3("min"), 
-          value = NULL
-        )
-      )
-    ),
-    
-    fluidRow(
-      box(
-        helpText(
-          "Some helpful text", 
-          label = h3("Description")
+      # Plot tab content
+      tabItem(
+        tabName = "plot",
+        fluidRow(
+          box(
+            plotOutput(
+              "graph", 
+              height = 250
+            )
+          ),
+          
+          box(
+            title = "Controls",
+            sliderInput(
+              "slider", 
+              "Number of observations:", 1, 100, 50
+            )
+          )
         )
       )
     )
