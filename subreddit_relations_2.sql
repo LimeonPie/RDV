e75c57f03b9b326bb80a4cdfc6d8b21e42dc62a1
anjovis <eemeli.ristimella@gmail.com>
2016-02-04 01:38:10 +0200

    well it seems the query somehow magically works

--- subreddit_relations_2.sql
+++ subreddit_relations_2.sql
@@ -1,12 +1,13 @@
-SELECT b.subreddit AS subreddit_a, a.subreddit AS subreddit_b, b.author, a.author, b.authors, FLOOR(100*COUNT(*)/b.authors)
+SELECT b.subreddit AS subreddit_a, b.authors AS authors_in_sub_a, a.subreddit AS subreddit_b, FLOOR(100*COUNT(*)/b.authors) AS percent, COUNT(*)
 FROM
 ((SELECT DISTINCT (author), subreddit FROM rawdata ORDER BY subreddit) AS a)
 JOIN 
 ((SELECT t1.author AS author, t1.subreddit AS subreddit, t2.authors AS authors
-FROM ((SELECT DISTINCT author, subreddit FROM rawdata ) AS t1)
-JOIN (SELECT authors FROM authors_in_subreddits) AS t2
-GROUP BY subreddit
- ) AS b)
+FROM (SELECT DISTINCT author, subreddit FROM rawdata ) AS t1
+JOIN (SELECT subreddit, count(distinct author) AS authors FROM rawdata GROUP BY subreddit) AS t2
+WHERE t1.subreddit=t2.subreddit
+GROUP BY subreddit, author
+ ) AS b /*b is a table which includes every distinct author in every subreddits and also the amount of distinct authors in every subreddit*/)
 ON a.author=b.author
 WHERE a.subreddit!=b.subreddit 
-GROUP BY 1,2;
+GROUP BY 1,3;
